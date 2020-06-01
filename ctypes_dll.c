0dc7527dad9c307842a01b909d453f092eb4f21e
yr.deng <yr.deng@kneron.us>
2020-06-01 15:28:17 +0800

    reformat code

--- ctypes_dll.c
+++ ctypes_dll.c
@@ -9,20 +9,20 @@
 #define USBD_ENDPOINT_DBG_OUT 0x03
 #define USBD_ENDPOINT_DBG_IN 0x84
 
-int test_value(int* pValue)
+int test_value(int *pValue)
 {
     *pValue = 999;
     return *pValue;
 }
 
-void test_buf(void* buf)
+void test_buf(void *buf)
 {
-    byte* buffer = (byte*)buf;
+    byte *buffer = (byte *)buf;
     *buffer = 11;
     *(buffer + 1) = 22;
 }
 
-size_t test_struct_array(struct bounding_box_s** ppBoxes, size_t boxes_len)
+size_t test_struct_array(struct bounding_box_s **ppBoxes, size_t boxes_len)
 {
     struct bounding_box_s box1 = {
         .x1 = 10.1f,
@@ -30,16 +30,14 @@ size_t test_struct_array(struct bounding_box_s** ppBoxes, size_t boxes_len)
         .x2 = 20.1f,
         .y2 = 20.1f,
         .score = 20.0f,
-        .class_num = 1
-    };
+        .class_num = 1};
     struct bounding_box_s box2 = {
         .x1 = 30.2f,
         .y1 = 30.2f,
         .x2 = 100.1f,
         .y2 = 100.1f,
         .score = 60.0f,
-        .class_num = 2
-    };
+        .class_num = 2};
     (*ppBoxes)[0] = box1;
     (*ppBoxes)[1] = box2;
     return boxes_len;
--- include/ctypes_dll.h
+++ include/ctypes_dll.h
@@ -12,14 +12,14 @@ typedef unsigned char byte;
 
 struct bounding_box_s
 {
-    float x1;      // top-left corner: x
-    float y1;      // top-left corner: y
-    float x2;      // bottom-right corner: x
-    float y2;      // bottom-right corner: y
-    float score;   // probability score
+    float x1;          // top-left corner: x
+    float y1;          // top-left corner: y
+    float x2;          // bottom-right corner: x
+    float y2;          // bottom-right corner: y
+    float score;       // probability score
     int32_t class_num; // class # (of many) with highest probability
 };
 
-WINDOWS_DLL_DEFINE int test_value(int* pValue);
-WINDOWS_DLL_DEFINE void test_buf(void* buf);
-WINDOWS_DLL_DEFINE size_t test_struct_array(struct bounding_box_s** ppBoxes, size_t boxes_len);
+WINDOWS_DLL_DEFINE int test_value(int *pValue);
+WINDOWS_DLL_DEFINE void test_buf(void *buf);
+WINDOWS_DLL_DEFINE size_t test_struct_array(struct bounding_box_s **ppBoxes, size_t boxes_len);
